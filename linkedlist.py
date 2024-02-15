0cd06a50858534bd89a7bf7666f73752266094f4
musaarfah <123568805+musaarfah@users.noreply.github.com>
2024-02-16 01:31:46 +0500

    Node and LL class init

---
+++ linkedlist.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.ref=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+
+    
+        
+        
