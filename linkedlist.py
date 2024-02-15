5e2b34424f9c6095ce203ee3e7d39e037851dd67
musaarfah <123568805+musaarfah@users.noreply.github.com>
2024-02-16 01:49:15 +0500

    list first draft

--- linkedlist.py
+++ linkedlist.py
@@ -7,6 +7,28 @@ class LinkedList:
     def __init__(self):
         self.head=None
 
-    
+    def insert_at_start(self,data):
+        new_node=Node(data)
+        if self.head is None:
+            self.head=new_node
+        else:
+            new_node.ref=self.head
+            self.head=new_node
+
+    def print_llist(self):
+        if self.head is None:
+            print('Linked List is empty')
+
+        else:
+            current=self.head
+            print(current)
+            while current is not None:
+                print(current.data,end=' ')
+                current=current.ref
+
+
         
-        
+def main():
+    l1=LinkedList()
+    l1.insert_at_start(10)
+    l1.print_llist()
